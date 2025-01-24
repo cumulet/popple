fb52f040d9b1f51e4ac3b3ddeb26829cb20dffc3
GuillaumePoly <guillaumeppauli@gmail.com>
2025-01-24 22:28:36 +0100

    hierarchy update

---
+++ src/entities/bubble/bubble_interactor.gd
@@ -0,0 +1,12 @@
+class_name BubbleInteractor
+extends RigidBody3D
+
+
+
+
+func _on_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_mouse_exited() -> void:
+	pass # Replace with function body.
