98b69ff5cea0e80179be18b87af1f07ca8b6d659
angelpatane <angelpatane9@gmail.com>
2025-01-25 03:22:48 +0100

    Implemented working NextBubbleChooser

--- src/entities/bubble/next_bubble_chooser.gd
+++ src/entities/bubble/next_bubble_chooser.gd
@@ -2,15 +2,27 @@ extends Node3D
 
 @export var bubble_spawner: BubbleSpawner
 
-var current_bubble: Bubble:
-	set(new_value):
-		current_bubble = new_value
-		Utils.set_albedo_in_mesh(mesh_current_bubble, Utils.get_albedo_in_mesh(current_bubble.mesh_bubble))
+#var current_bubble: Bubble:
+	#set(new_value):
+		#if is_instance_valid(new_value):
+			#current_bubble = new_value
+			#Utils.set_albedo_in_mesh(mesh_current_bubble, Utils.get_albedo_in_mesh(current_bubble.mesh_bubble))
 
-var next_bubble: Bubble:
+var current_bubble_color: Color:
 	set(new_value):
-		next_bubble = new_value
-		Utils.set_albedo_in_mesh(mesh_next_bubble, Utils.get_albedo_in_mesh(next_bubble.mesh_bubble))
+		current_bubble_color = new_value
+		Utils.set_albedo_in_mesh(mesh_current_bubble, current_bubble_color)
+
+var next_bubble_color: Color:
+	set(new_value):
+		next_bubble_color = new_value
+		Utils.set_albedo_in_mesh(mesh_next_bubble, next_bubble_color)
+
+#var next_bubble: Bubble:
+	#set(new_value):
+		#if is_instance_valid(new_value):
+			#next_bubble = new_value
+			#Utils.set_albedo_in_mesh(mesh_next_bubble, Utils.get_albedo_in_mesh(next_bubble.mesh_bubble))
 
 
 @onready var mesh_current_bubble: MeshInstance3D = $CurrentBubble/MeshCurrentBubble
@@ -19,29 +31,31 @@ var next_bubble: Bubble:
 func _ready() -> void:
 	update_bubbles_in_scene()
 	choose_current_bubble()
-	choose_next_bubble()
+	choose_next_bubble_color()
 
 func update_bubbles_in_scene():
 	for bubble: Bubble in get_tree().get_nodes_in_group("Bubbles"):
-		bubble.popped.connect(_on_bubble_popped.bind(bubble))
+		if bubble.popped.is_connected(_on_bubble_popped) == false:
+			bubble.popped.connect(_on_bubble_popped.bind(bubble))
 
 func _on_bubble_popped(_bubble: Bubble):
-	if Utils.get_albedo_in_mesh(_bubble.mesh_bubble) == Utils.get_albedo_in_mesh(current_bubble.mesh_bubble):
-		print("Good job, pick the next bubble")
-		current_bubble = next_bubble
-		choose_next_bubble()
-	else:
-		print("Bad job, you didn't pick the same as the current bubble")
-		choose_next_bubble()
+	var popped_bubble_color: Color = Utils.get_albedo_in_mesh(_bubble.mesh_bubble)
+	if popped_bubble_color == current_bubble_color:
+		print("Good job")
+		current_bubble_color = next_bubble_color
+	_bubble.remove_from_group("Bubbles")
 	_bubble.queue_free()
+	choose_next_bubble_color()
 	update_bubbles_in_scene()
 
 func choose_current_bubble():
-	current_bubble = get_tree().get_nodes_in_group("Bubbles").pick_random()
+	var tmp_bubble: Bubble = get_tree().get_nodes_in_group("Bubbles").pick_random()
+	current_bubble_color = Utils.get_albedo_in_mesh(tmp_bubble.mesh_bubble)
 
-func choose_next_bubble():
-	var tmp_next_bubble: Bubble = get_tree().get_nodes_in_group("Bubbles").pick_random()
-	if tmp_next_bubble != current_bubble:
-		next_bubble = tmp_next_bubble
-	else:
-		choose_next_bubble()
+func choose_next_bubble_color():
+	while true:
+		var tmp_next_bubble: Bubble = get_tree().get_nodes_in_group("Bubbles").pick_random()
+		var tmp_next_color = Utils.get_albedo_in_mesh(tmp_next_bubble.mesh_bubble)
+		if tmp_next_color != current_bubble_color:
+			next_bubble_color = tmp_next_color
+			break
