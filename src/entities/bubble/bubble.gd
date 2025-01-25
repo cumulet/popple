25d48217bf2abd7b4b7d686f576978a261328380
GuillaumePoly <guillaumeppauli@gmail.com>
2025-01-25 01:02:13 +0100

    merge conflict

--- src/entities/bubble/bubble.gd
+++ src/entities/bubble/bubble.gd
@@ -1,6 +1,8 @@
 class_name Bubble
 extends RigidBody3D
 
+signal popped
+
 const color_bubble_dictionary: = {
 	"Yellow": Color.YELLOW,
 	"Red": Color.RED,
@@ -59,6 +61,8 @@ func _randomize_scale() -> Vector3:
 
 func _on_trigger_area_3d_mouse_entered() -> void:
 	hover(0.1)
+	popped.emit()
+	
 
 func _on_trigger_area_3d_mouse_exited() -> void:
 	hover(-0.1)
