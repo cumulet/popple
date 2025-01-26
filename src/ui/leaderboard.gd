206a0b048880cffa1c7a18f2f9596398952c20ab
angelpatane <angelpatane9@gmail.com>
2025-01-26 13:28:58 +0100

    Leaderboard Working

--- src/ui/leaderboard.gd
+++ src/ui/leaderboard.gd
@@ -4,15 +4,14 @@ extends Control
 
 
 func _ready() -> void:
-	var points = points_node.get_children()
-	for point in points:
-		point.visible = false
+	for point: Label in points_node.get_children():
+		point.hide()
 	ScoreManager.leaderboard_score_updated.connect(_leaderboard_score_updated)
 
 func _leaderboard_score_updated(_new_scores: Array[float]):
 	for point: Label in points_node.get_children():
 		print("leaderboard updated")
 		print(point.get_index())
-		if point.get_index() >= _new_scores.size() - 1:
+		if point.get_index() < _new_scores.size():
 			point.show()
 			point.text = str(_new_scores[point.get_index()])
